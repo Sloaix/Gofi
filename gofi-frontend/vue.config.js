5b91bb3e2ad293742ded7971ff5048b1349abd01
Sloaix <faith.epiphone@qq.com>
2019-11-28 11:29:30 +0800

    refactor: remove vue cndjs

--- gofi-frontend/vue.config.js
+++ gofi-frontend/vue.config.js
@@ -6,33 +6,6 @@ function resolve (dir) {
   return path.join(__dirname, dir)
 }
 
-/**
- * check production or preview(pro.loacg.com only)
- * @returns {boolean}
- */
-function isProd () {
-  return process.env.NODE_ENV === 'production'
-}
-
-const assetsCDN = {
-  css: [],
-  // https://unpkg.com/browse/vue@2.6.10/
-  js: [
-    '//cdn.jsdelivr.net/npm/vue@2.6.10/dist/vue.min.js',
-    '//cdn.jsdelivr.net/npm/vue-router@3.1.3/dist/vue-router.min.js',
-    '//cdn.jsdelivr.net/npm/vuex@3.1.1/dist/vuex.min.js',
-    '//cdn.jsdelivr.net/npm/axios@0.19.0/dist/axios.min.js'
-  ]
-}
-
-// webpack build externals
-const prodExternals = {
-  vue: 'Vue',
-  'vue-router': 'VueRouter',
-  vuex: 'Vuex',
-  axios: 'axios'
-}
-
 // vue.config.js
 const vueConfig = {
   configureWebpack: {
@@ -40,9 +13,7 @@ const vueConfig = {
     plugins: [
       // Ignore all locale files of moment.js
       new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/)
-    ],
-    // if prod is on, add externals
-    externals: isProd() ? prodExternals : {}
+    ]
   },
 
   chainWebpack: (config) => {
@@ -64,15 +35,6 @@ const vueConfig = {
       .options({
         name: 'assets/[name].[hash:8].[ext]'
       })
-
-    // if prod is on
-    // assets require on cdn
-    if (isProd()) {
-      config.plugin('html').tap(args => {
-        args[0].cdn = assetsCDN
-        return args
-      })
-    }
   },
 
   css: {
@@ -94,14 +56,6 @@ const vueConfig = {
   devServer: {
     // development server port 8000
     port: 8000
-    // If you want to turn on the proxy, please remove the mockjs /src/main.jsL11
-    // proxy: {
-    //   '/api': {
-    //     target: 'https://mock.ihx.me/mock/5baf3052f7da7e07e04a5116/antd-pro',
-    //     ws: false,
-    //     changeOrigin: true
-    //   }
-    // }
   },
 
   // disable source map in production
