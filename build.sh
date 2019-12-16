d5323a286ba86cd381cfa2e4acf8ebf931b97ed8
Sloaix <faith.epiphone@qq.com>
2019-12-16 18:04:09 +0800

    chore: reactor release workflow to fix #4

--- build.sh
+++ build.sh
@@ -1,19 +1,42 @@
 #!/bin/bash
+function gofilog() {
+    echo 【Gofi Build】====\> $@
+}
+
+go_path=$(go env GOPATH)
+go_bin_dir=$go_path/bin
 build_dir=$(pwd)
+build_dir_name=$(basename $build_dir)
+correct_build_dir=$go_path/src/$build_dir_name
 client_dir=$(pwd)/gofi-frontend
 server_dir=$(pwd)/gofi-backend
-go_bin_dir=$(go env GOPATH)/bin
-tag=$(git describe --tags $(git rev-list --tags --max-count=1))
+
 ENV=$1
 ENV="${ENV:-product}"
 
+gofilog corrent build dir is $correct_build_dir, current build dir is $build_dir
 
-echo Gofi: build enviroment is $ENV
+if [ "$build_dir" != "$correct_build_dir" ]; then
+    gofilog build failed, you must place project at $correct_build_dir
+    exit
+fi
 
-function usage() {
-    echo "Usage: $0 [-s <45|90>] [-p <string>]" 1>&2
-    exit 1
+gofilog build for $ENV
+
+# 如果最新的commit有tag,返回tag，否则直接返回commit id
+function getVersion() {
+    lastCommit=$(git rev-parse HEAD)
+    lastTagCommit=$(git rev-list --tags --max-count=1)
+    if [ "$lastCommit" == "$lastTagCommit" ]; then
+        #last tag
+        echo $(git describe --tags $(git rev-list --tags --max-count=1))
+    else
+        # last commit short hash
+        echo $(git rev-parse --short=8 HEAD)
+    fi
 }
+
+gofilog build version is $(getVersion)
 
 function bindata() {
     if ! [ -x "$(command -v $go_bin_dir/go-bindata)" ]; then
@@ -24,25 +47,25 @@ function bindata() {
 
 function xgo() {
     if ! [ -x "$(command -v $go_bin_dir/xgo)" ]; then
-        go get src.techknowlogick.com/xgo
+        go get github.com/karalabe/xgo
     fi
-    $go_bin_dir/xgo -go=go-1.13.4 -out=gofi-$tag -tags=''$ENV'' -ldflags='-X gofi/context.version='$tag'' --dest=./output --targets=windows/amd64,darwin/amd64,linux/amd64,linux/arm,android/arm $1
+    $go_bin_dir/xgo -out=gofi -tags=''$ENV'' -ldflags='-w -s -X gofi/context.version='$(getVersion)'' --dest=./output --targets=windows/amd64,darwin/amd64,linux/amd64,linux/arm,android/arm $1
 }
 
 function beforeBuild() {
-    echo "before build, cleaning..."
+    gofilog "before build, cleaning..."
     rm -rf $build_dir/output
     rm -rf $server_dir/output
 }
 
 function buildClient() {
-    echo "start building client for Gofi..."
+    gofilog "start building client for Gofi..."
     cd $client_dir
     yarn install && yarn run lint --no-fix && yarn run build
 }
 
 function buildServer() {
-    echo "start build server for Gofi"
+    gofilog "start build server for Gofi"
     cd $build_dir
     mv $client_dir/dist $server_dir/public
     cd $server_dir
@@ -54,9 +77,9 @@ function buildServer() {
 }
 
 function afterBuild() {
-    echo "after build, cleaning..."
+    gofilog "after build, cleaning..."
     rm -rf $server_dir/public
-    echo "Build complete. The build product has been exported to the directory $build_dir/output"
+    gofilog "Build complete. The build product has been exported to the directory $build_dir/output"
     cd $build_dir/output
     ls -al
 }
