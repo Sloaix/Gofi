d6b45447005d8d14278069cfff67d002aad24204
Sloaix <faith.epiphone@qq.com>
2021-08-07 11:05:39 +0800

    refactor: optimize ui of setting page

--- gofi-frontend/src/pages/admin/Setting.tsx
+++ gofi-frontend/src/pages/admin/Setting.tsx
@@ -1,10 +1,21 @@
+import { AiOutlineLock } from '@hacknug/react-icons/ai'
 import { MdCheck, MdClose, MdEdit } from '@hacknug/react-icons/md'
-import { RiSettings2Line } from '@hacknug/react-icons/ri'
+import {
+    RiComputerFill,
+    RiComputerLine,
+    RiDatabase2Fill,
+    RiDatabase2Line,
+    RiDoorLockLine,
+    RiSettings2Line,
+    RiShieldKeyholeLine,
+    RiShieldUserLine,
+} from '@hacknug/react-icons/ri'
 import { RouteComponentProps } from '@reach/router'
 import { observer } from 'mobx-react-lite'
 import React, { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import repo from '../../api/repository'
+import logo from '../../assets/logo.svg'
 import Button from '../../components/Button'
 import Input from '../../components/form/Input'
 import MainLayout from '../../components/layouts/MainLayout/Index'
@@ -48,7 +59,7 @@ const Setting: React.FC<IProps> = (props) => {
     }
 
     const itemClasses = 'bg-gray-50 px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6'
-    const itemTitleClasses = 'text-sm font-medium text-gray-500'
+    const itemTitleClasses = 'text-sm font-medium text-gray-500 flex items-center space-x-1'
     const itemFieldClasses = 'mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2'
 
     return (
@@ -60,8 +71,11 @@ const Setting: React.FC<IProps> = (props) => {
                 </div>
                 <div className="border-t border-gray-200">
                     <dl>
+                        {/* 仓库路径 */}
                         <div className={itemClasses}>
-                            <dt className={itemTitleClasses}>{t('pages.admin.setting.label.storage-path')}</dt>
+                            <dt className={itemTitleClasses}>
+                                <RiDatabase2Line /> <span>{t('pages.admin.setting.label.storage-path')}</span>
+                            </dt>
                             <dd className={itemFieldClasses}>
                                 {appStore.config?.customStoragePath
                                     ? appStore.config?.customStoragePath
@@ -69,24 +83,36 @@ const Setting: React.FC<IProps> = (props) => {
                             </dd>
                         </div>
 
+                        {/* 工作目录 */}
                         <div className={itemClasses}>
-                            <dt className={itemTitleClasses}>{t('pages.admin.setting.label.gofi-workdir')}</dt>
+                            <dt className={itemTitleClasses}>
+                                <RiComputerLine />
+                                <span> {t('pages.admin.setting.label.gofi-workdir')}</span>
+                            </dt>
                             <dd className={itemFieldClasses}>{appStore.config?.appPath}</dd>
                         </div>
+                        {/* 版本 */}
                         <div className={itemClasses}>
-                            <dt className={itemTitleClasses}>{t('pages.admin.setting.label.version')}</dt>
+                            <dt className={itemTitleClasses}>
+                                <img src={logo} className="h-3" />
+                                <span>{t('pages.admin.setting.label.version')}</span>
+                            </dt>
                             <dd className={itemFieldClasses}>{appStore.config?.version}</dd>
                         </div>
-                        {/* <div className={itemClasses}>
-                            <dt className={itemTitleClasses}>用户类型</dt>
-                            <dd className={itemFieldClasses}>{FormatUtil.formatUserType(userStore.user?.roleType)}</dd>
-                        </div> */}
+                        {/* 用户名 */}
                         <div className={itemClasses}>
-                            <dt className={itemTitleClasses}>{t('pages.admin.setting.label.username')}</dt>
+                            <dt className={itemTitleClasses}>
+                                <RiShieldUserLine />
+                                <span>{t('pages.admin.setting.label.username')}</span>
+                            </dt>
                             <dd className={itemFieldClasses}>{userStore.user?.username}</dd>
                         </div>
+                        {/* 密码 */}
                         <div className={itemClasses}>
-                            <dt className={itemTitleClasses}>{t('pages.admin.setting.label.password')}</dt>
+                            <dt className={itemTitleClasses}>
+                                <RiDoorLockLine />
+                                <span>{t('pages.admin.setting.label.password')}</span>
+                            </dt>
                             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 flex space-x-2">
                                 <Input
                                     disable={!isFormMode}
