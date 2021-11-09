9e2f63ae39ea961ec62543f7f8788fd6d6230749
Sloaix <faith.epiphone@qq.com>
2021-11-09 14:47:11 +0800

    refactor: change version string to 'dev' if in the development environment

--- gofi-backend/db/version.go
+++ gofi-backend/db/version.go
@@ -1,4 +1,12 @@
 package db
 
+import "gofi/env"
+
 //version ,will be replaced at compile time by [-ldflags="-X 'gofi/db.version=vX.X.X'"]
 var version = "UNKOWN VERSION"
+
+func init() {
+	if env.IsDevelop() {
+		version = "DEV"
+	}
+}
